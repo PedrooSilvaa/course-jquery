11f8e23dd0740982d2888df9c72876950b14df53
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 11:32:12 -0300

    class on selectors using basic filters

---
+++ seletores/seletores_atrinutos.js
@@ -0,0 +1,18 @@
+            // Verificando se possui um atributo
+            // $('.header-border li [href]').text('Link')
+
+            // Verificando se possui mais de um atributo
+            // $('.header-border li [href][title]').text('Link')
+            
+            // Verificando um valor para o atributo
+            // $('.header-border li [href="about.html"]').text()
+            
+            // Verificando os atributos que começa com uma letra especifica
+            // $('.header-border li [href^="a"]').text()
+            
+            // Verificando os atributos que terminam com uma expressão especifica
+            // $('.header-border li [href$="a"]').text()
+            
+            // Verificando os atributos que possuem a expressão informada
+            // $('.header-border li [href*="about"]').text()
+

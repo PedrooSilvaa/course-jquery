0c738e6cdf0a56a4c727e50405822b61b83d2206
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 11:10:10 -0300

    class on selectors using hierarchy

---
+++ seletores/seletores_basicos.js
@@ -0,0 +1,14 @@
+//Nome da tag -> $('h1').css('color', 'blue')
+            
+            //ID -> $('#logo').html("Negocios")
+            
+            //Selecionando pela classe $('.paragrafo').animate({
+            //     fontSize: '50px',
+            //     opacity: 0.7
+            // }, 5000)
+
+            //Seletor Composto
+            //$('header nav.menu').css('border', 'solid 1px orange')
+            
+            //Multiplos Seletores
+            //$('form, section').css('background-color', '#ccc')

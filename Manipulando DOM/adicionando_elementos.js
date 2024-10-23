e9362ce3f658c75ac152a8514a37c5bd5485205a
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 15:17:34 -0300

    feat: class about DOM, removing elements

---
+++ Manipulando DOM/adicionando_elementos.js
@@ -0,0 +1,30 @@
+
+// Adicionando um elemento ao final de outro
+// $('.menu ul').append('<li><a href="#">News</a></li>')
+
+// Adicionando um elemento ao final de outro em outra sintaxe
+// $('<li><a href="#">News</a></li>').appendTo('.menu ul')
+
+// Adicionando um elemento no inicio de outro
+// $('.menu ul').prepend('<li><a href="#">News</a></li>')
+
+// Adicionando um elemento no inicio de outro em outra sintaxe
+// $('<li><a href="#">News</a></li>').prependTo('.menu ul');
+
+// Aicionando um elemento após outro 
+// $('.paragrafo').after('<h2>HCode Treinamentos</h2>')
+
+// Aicionando um elemento após outro, cim outra sintaxe
+// $('<h2>HCode Treinamentos</h2>').insertAfter('.paragrafo')
+
+// Aicionando um elemento antes de outro 
+// $('.paragrafo').before('<h2>HCode Treinamentos</h2>')
+
+// Aicionando um elemento antes outro, cim outra sintaxe
+// $('<h2>HCode Treinamentos</h2>').insertBefore('.paragrafo')
+
+// Adicionando um elemento em volta de outro
+// $('.cars_list li').wrap('<i></i>')
+
+// Adicionando um elemento em volta do conteudo de outro
+// $('.cars_list li').wrapInner('<i></i>')

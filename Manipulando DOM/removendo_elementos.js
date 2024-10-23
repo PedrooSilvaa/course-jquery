bb662c162bceea3f6fc15bbb902259df086c6ae0
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 15:21:34 -0300

    feat: class about DOM, cloning elements

---
+++ Manipulando DOM/removendo_elementos.js
@@ -0,0 +1,15 @@
+// Removendo elementos
+// $('.cars_list li:eq(1)').remove();
+
+// Removendo conteudo de elementos
+// $('.cars_list li:eq(1)').empty();
+
+// Removendo elementos mas mantendo seus eventos associados
+// $('.paragrafo').click(function(){
+//    $(this).html("Você clicou em mim")
+// })
+// let txt = $('.paragrafo').detach();
+// $('section main p').before(txt)
+
+// Removendo os elementos em volta de outro
+// $('.cars_list li').unwrap()

f824233a638799d7a4ee1403c3be63068e633f0b
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 15:08:16 -0300

    feat: class about DOM, adding elements

---
+++ Manipulando DOM/manipulando_posicoes.js
@@ -0,0 +1,14 @@
+// Retonando ou definindo a posição de um elemento
+// $('form, field:eq(0)').offset({
+//     top: 5,
+//     left:100
+// });
+
+// Retonando a  posção de um elemento se bsaeando no elemento pai
+// $('form, field:eq(0)').position();
+
+// Retornando ou definindo a posicao da barra de rolagem vertical
+// $(document).scrollTop(400)
+
+// Retornando ou definindo a posicao da barra de rolagem horizontal
+// $(document).scrollLeft(400)

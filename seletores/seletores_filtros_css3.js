88ba6c89dc11985205868d9218dba6abbd80eb5b
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 12:19:47 -0300

    class on selectors for content filters

---
+++ seletores/seletores_filtros_css3.js
@@ -0,0 +1,29 @@
+// Selecionando o ultimo elemento, mesmo que seja mais de um caso
+            // $(document).ready(function(){
+            //     $('.cars_list li:last-child').css('text-decoration', 'underline')
+            // })
+
+            // Selecionando o primeiro elemento, mesmo que seja mais de um caso
+            // $(document).ready(function(){
+            //     $('.cars_list li:first-child').css('text-decoration', 'underline')
+            // })
+            
+            // Selecionando os elemento de maneira alternada (par)
+            // $(document).ready(function(){
+            //     $('.cars_list li:nth-child(even)').css('text-decoration', 'underline')
+            // })
+            
+            // Selecionando os elemento de maneira alternada (impar)
+            // $(document).ready(function(){
+            //     $('.cars_list li:nth-child(odd)').css('text-decoration', 'underline')
+            // })
+            
+            // Selecionando um elemento em uma posicao espeecifica, mesmo que seja mais de um
+            // $(document).ready(function(){
+            //     $('.cars_list li:nth-child(1)').css('text-decoration', 'underline')
+            // })
+            
+            // Selecionando um elemento em uma posicao espeecifica, comecando do ultimo elemento
+            // $(document).ready(function(){
+            //     $('.cars_list li:nth-last-child(1)').css('text-decoration', 'underline')
+            // })

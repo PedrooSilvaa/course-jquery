4d5833005daf48d2e60e5a9a17f21303be48ab99
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 11:43:40 -0300

    class on selectors using css3 filters

---
+++ seletores/seletores_filtros_basicos.js
@@ -0,0 +1,25 @@
+ // Selecionando o último elemento
+            // $('.cars_list li:last').click(function(){
+            //     alert('ok');
+            // })
+
+            // Selecionando o primeiro elemento
+            // $('.cars_list li:first').click(function(){
+            //     alert('ok numero 1');
+            // })
+
+            // Selecionando elementos de maneira alternada, comecando pelos impares
+            // $('.cars_list li:even').css('background-color', '#ccc')
+
+            // Selecionando elementos de maneira alternada, comecando pelos impares
+            // $('.cars_list li:odd').css('background-color', 'red')
+
+            // Selecionando um elemento em uma posição esppecifica (baseado em um array)
+            // $('.cars_list li:eq(0)').css('font-size', '40px')
+            
+            // Selecionando todos os elemento abaixo de outro
+            // $('.cars_list li:gt(0)').css('font-size', '40px')
+            
+            // Selecionando todos os elemento acima de outro
+            // $('.cars_list li:lt(3)').css('font-size', '40px')
+            

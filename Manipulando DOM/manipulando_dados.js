fb7ab9e3e76f4f4087e013c51ad6d1b3560fe766
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 14:28:05 -0300

    feat: class about DOM, manipulating dimensions

---
+++ Manipulando DOM/manipulando_dados.js
@@ -0,0 +1,6 @@
+// Retonando ou deifinindo o dado de um elemento
+// $('.cars_list li:first').data('ano', '1990');
+
+// Removendo dados de um elemento
+// $('.cars_list li:first').removeData('ano');
+

6b576c260207f1c4fc1da199d6e04b0595b07d7f
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 14:05:56 -0300

    feat: class about DOM, manipulating attributes

---
+++ Manipulando DOM/manipulando_classes.js
@@ -0,0 +1,21 @@
+// Adicionando uma classe
+// $('header .menu ul li:last a').addClass('stylized_menu')
+
+// Removendo uma classe
+// $('header .menu ul li:last a').removeClass('stylized_menu')
+
+// Verificando se o atributo possui uma classe
+// $('header .menu ul li:last a').hasClass('stylized_menu')
+
+// $('header .menu ul li a').click(function(event){
+//     event.preventDefault();
+
+//     $(this).toggleClass('stylized_menu')
+// })
+
+// Verificando cada elemento se possui classe
+// $('p').each(function(){
+//     if($(this).hasClass('paragrafo')){
+//         $(this).append(`<span style="color: red"> - Eu tenho a classe </span>`)
+//     }
+// })

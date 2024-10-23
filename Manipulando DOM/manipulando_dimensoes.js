4a93b809892868ed2b2b389f89b065602f42d818
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 14:37:42 -0300

    feat: class about DOM, manipulating positions

---
+++ Manipulando DOM/manipulando_dimensoes.js
@@ -0,0 +1,11 @@
+// Retonando ou deifinindo a largura de um elemento
+// $('.paragrafo').width(500)
+
+// Retonando ou deifinindo a largura de um elemento, levando em conta o padding
+// $('.paragrafo').inneridth()
+
+// Retonando ou deifinindo a altura de um elemento
+// $('.paragrafo').height(500)
+
+// Retonando ou deifinindo a altura de um elemento, levando em conta o padding
+// $('.paragrafo').innerHeight()

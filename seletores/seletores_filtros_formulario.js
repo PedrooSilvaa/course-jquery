98de51f7ccb1ce68e823522e249be4edcde884bf
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 13:23:13 -0300

    class on selectors using form filters

---
+++ seletores/seletores_filtros_formulario.js
@@ -0,0 +1,30 @@
+
+            // $('#register :submit').click(function(event){
+            //     event.preventDefault();
+            //     alert("Você clicou no botão submit")
+            // })
+
+            // $('input').focus(function(){
+            //     $('input:focus').val('Olá você clicou aqui')
+            // })
+
+            // $('[type=radio]').change(function(){
+
+            //     let value = $('input:checked').val();
+            //     let gender = (value == 'M') ? 'Masculino' : 'Feminino';
+
+            //     alert(`Voce selecionou o sexo ${gender}`)
+
+            // })
+
+            // Selecionando um botão
+            // $('#register :button').text()
+
+            // Selecionando um checkbox
+            // $('#register :checkbox').trigger('click')
+
+            // Selecionando os elementos "checados"
+            // $('input:checked').val();
+
+            // Selecionando os elementos desabilitados
+            // $('#register :disabled').val("pedro@email.com").prop('disabled', false);
---
+++ seletores/seletores_visibilidade.js
@@ -0,0 +1,21 @@
+    // Selecionando os elementos que estão "invisiveis"
+    // $('.cars_list li:hidden').show();
+
+    // Selecionando os elementos visiveis
+    // $('.cars_list li:visible').hide();
+
+    // Exibindo e Ocultando carros com um unico botão
+    // $(document).ready(function(){
+    //     $('#toggle_cars').click(function(){
+    //         let hiddenCars = $('.cars_list li:hidden').length
+    //         let method = hiddenCars > 0 ? 'show' : 'hide';
+
+    //         if(hiddenCars > 0){
+    //             method = 'show'
+    //         }else{
+    //             method = 'hide'
+    //         }
+
+    //         eval(`$('.cars_list li').${method}();`)
+    //     })
+    // })

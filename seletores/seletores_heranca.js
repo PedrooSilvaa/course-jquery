a349a34437e11d814f79f7f6bddb4d3e9514089b
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 11:19:11 -0300

    class on attribute selectors

---
+++ seletores/seletores_heranca.js
@@ -0,0 +1,17 @@
+// Elementos descendentes
+// $('main h1').css('color', 'red')
+
+// Elementos descendentes diretos
+// $('main>.title').css('border', 'solid 2px black')
+
+// Elemento irmao mais proximo
+// $('.paragrafo+p').css({
+//     'background-color': 'red',
+//     'color': 'white'
+// })
+
+// Todos elementos irmaos mais proximo
+// $('.paragrafo~p').css({
+//     'background-color': 'red',
+//     'color': 'white'
+// })

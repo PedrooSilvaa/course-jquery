2049461f7794f3c0a8f08ffdf147c20a6c81d6ef
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-23 13:55:57 -0300

    feat: class about DOM, manipulating classes

---
+++ seletores/seletores_nossos_filtros.js
@@ -0,0 +1,16 @@
+
+            // $.expr[':'].carsCentury21 = function(el){
+            //     return ($(el).data('ano') > 2000);
+            // }
+
+            // $.expr[':'].carsBeforeYear = $.expr.createPseudo(function(year){
+            //     return function(el){
+            //         return ($(el).data('ano') < year);
+            //     }
+            // })
+
+            //Criando no próprio filtro
+            // $('.cars_list li:carsCentury21').css('font-size', '30px')
+
+            //Criando no segundo filtro
+            // $('.cars_list li:carsBeforeYear(1950)').css('font-size', '30px')

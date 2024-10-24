415aca509a1b974dc8eefd7a272d6cee8209c472
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-23 22:22:00 -0300

    feat: class on form events

---
+++ eventos/eventos_teclado.js
@@ -0,0 +1,15 @@
+
+// Evento para quando pressionarmos uma tecla
+/* $('form [name=username]').keydown(function(event){
+    console.log(`Você apertou a tecla com um codigo ${event.keyCode}`)
+}) */
+
+// Evento para quando soltarmos uma tecla
+/* $('form [name=username]').keyup(function(event){
+    console.log(`Você apertou a tecla com um codigo ${event.key}`)
+}) */
+
+// Evento para quando soltarmos uma tecla
+/* $('form [name=username]').keypress(function(event){
+    console.log(`Você apertou a tecla com um codigo ${event.key}`)
+}) */
